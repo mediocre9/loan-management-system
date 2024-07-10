0b2a03d20e33a449437ebc93dd94d53b7bfe555c
mediocre9 <mirzafahadzia9@gmail.com>
2024-07-10 08:41:05 +0500

    feat: add user first and last name fields

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,35 @@
 import { model, Schema } from "mongoose";
 import bcrypt from "bcrypt";
-import isEmail from "validator/lib/isEmail.js";
+import validator from "validator";
 
 const userSchema = new Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, "first name is required!"],
+        validate: {
+            validator: validator.isAlpha,
+            message: props => `${props.value} should not contain any digits or symbols!!`
+        },
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, "last name is required!"],
+        validate: {
+            validator: validator.isAlpha,
+            message: props => `${props.value} should not contain any digits or symbols!!`
+        },
+    },
     email: {
         type: String,
         trim: true,
         unique: true,
         required: [true, "Email is required!"],
         validate: {
-            validator: isEmail,
+            validator: validator.isEmail,
             message: props => `${props.value} is not a valid email format!`
         },
     },
@@ -17,6 +37,18 @@ const userSchema = new Schema({
         type: String,
         trim: true,
         minLength: 8,
+        validate: {
+            validator: (_) => {
+                return validator.isStrongPassword(_, {
+                    minLength: 8,
+                    minNumbers: 1,
+                    minLowercase: 1,
+                    minSymbols: 1,
+                    minUppercase: 1
+                });
+            },
+            message: props => `Password '${props.value}' is not strong enough! Make sure your password is at least 8 characters long and includes at least one uppercase letter, one lowercase letter, one symbol, and one numeric value.`
+        },
         required: [true, "Password is required!"],
     },
     verified: {
